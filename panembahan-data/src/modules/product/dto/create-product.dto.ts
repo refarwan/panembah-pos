b031a12584bf6ce5f67b4db9790a4135f301d7db
Aan Refarwan <refarwan@gmail.com>
2024-08-15 15:23:47 +0700

    feat: add endpoint POST product/add-product

--- panembahan-data/src/modules/product/dto/create-product.dto.ts
+++ panembahan-data/src/modules/product/dto/create-product.dto.ts
@@ -1,6 +1,15 @@
 import { IsNotEmpty } from "class-validator";
 
 export class CreateProductDto {
+	@IsNotEmpty({ message: "ID Produk tidak boleh kosong" })
+	id: string;
+
 	@IsNotEmpty({ message: "Nama tidak boleh kosong" })
 	name: string;
+
+	@IsNotEmpty({ message: "Satuan tidak boleh kosong" })
+	satuan: string;
+
+	@IsNotEmpty({ message: "Harga tidak boleh kosong" })
+	price: string;
 }
--- panembahan-data/src/modules/product/product.service.ts
+++ panembahan-data/src/modules/product/product.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class ProductService {
+	constructor(private prismaService: PrismaService) {}
+
 	async createProduct(): Promise<{ message: string }> {
+		// const productIdIsUnavailable = await this.prismaService.product.count({
+		//     where: {
+		//         id
+		//     }
+		// })
 		return { message: "Berhasil menambahkan produk" };
 	}
 }
